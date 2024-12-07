78bd4a273a8ed54afdbc0c17147afca9a32a4c34
L1ate <kirillserdyucov67@gmail.com>
2024-12-07 19:58:51 +0300

    Collision

--- game.js
+++ game.js
@@ -3,15 +3,14 @@ const ctx = canvas.getContext("2d");
 const body = document.querySelector('body');
 
 
-
 class GameSprite {
-  constructor(x, y, image, width, higth) {
+  constructor(x, y, image, width, height) {
     this.x = x;
     this.y = y;
-    this.image = new Image(width, higth);
+    this.image = new Image(width, height);
     this.image.src = image;
     this.width = width;
-    this.higth = higth;
+    this.height = height;
     this.moveRight = false;
     this.moveLeft = false;
     this.moveUp = false;
@@ -19,7 +18,7 @@ class GameSprite {
   }
 
   drawSprite() {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.higth);
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 }
 
@@ -65,17 +64,29 @@ class Player extends GameSprite {
   }
 
   movePlayer() {
-    if (this.moveRight) {
-      this.x += 2;
+    if (this.moveRight && this.x < canvas.width - this.width) {
+      this.x += 1;
+      if(isCollide(this, player2)){   
+        this.x -= 1;
+      }
     }
-    if (this.moveLeft) {
-      this.x -= 2;
+    if (this.moveLeft && this.x >= 0) {
+      this.x -= 1;
+      if(isCollide(this, player2)){   
+        this.x += 1;
+      }
     }
-    if (this.moveUp) {
-      this.y -= 2;
+    if (this.moveUp && this.y > 0) {
+      this.y -= 1;
+      if(isCollide(this, player2)){   
+        this.y += 1;
+      }
     }
-    if (this.moveDown) {
-      this.y += 2;
+    if (this.moveDown && this.y < canvas.height - this.height) {
+      this.y += 1;
+      if(isCollide(this, player2)){   
+        this.y -= 1;
+      }
     }
   }
 }
@@ -89,18 +100,23 @@ const isCollide = (obj1, obj2) => {
   return false;
 }
 
-const player = new Player(40, 40, 'sprite.png', 30, 30);
-const wall = new GameSprite(60, 40, 'wall.png', 30, 30);
+
+const player = new Player(40, 40, 'CM.png', 30, 30);
+const player2 = new GameSprite(0, 0, 'dog.jpg', 30, 30);
+//const wall = new GameSprite(60, 40, 'wall.png', 30, 30);
 player.startKeysEvents();
 player.image.onload = () => {
   const render = () => {
     ctx.clearRect(0, 0, 1000, 300);
     player.drawSprite();
-    wall.drawSprite();
+    player2.drawSprite();
+    //wall.drawSprite();
     player.movePlayer();
 
-    if (isCollide(player, wall)) {
-      console.log('Коснулось')
+    if (isCollide(player, player2)) {
+      player.x = 50;
+      player.y = 50;
+      console.log('Коснулось');
     }
 
     window.requestAnimationFrame(render);
