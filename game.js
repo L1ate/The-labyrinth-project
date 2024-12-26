6ad008c2c856ecb546dded4e293bd9dccdaddb7d
LikerK <nagorniykirill@gmail.com>
2024-12-26 17:28:11 +0400

    add move enemy

--- game.js
+++ game.js
@@ -3,29 +3,34 @@ const ctx = canvas.getContext("2d");
 const body = document.querySelector("body");
 // const game = document.querySelector('.game');
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // Максимум не включается, минимум включается
+}
+
 class GameSprite {
   constructor(x, y, image, width, height) {
     this.x = x;
     this.y = y;
-    this.image = document.createElement('img');
-    this.image.style.position = 'absolute';
-    this.image.setAttribute('src', image);
-    this.image.style.width = `${width}px`
-    this.image.style.width = `${height}px`
+    this.image = document.createElement("img");
+    this.image.style.position = "absolute";
+    this.image.setAttribute("src", image);
+    this.image.style.width = `${width}px`;
+    this.image.style.width = `${height}px`;
     this.width = width;
     this.height = height;
     this.moveRight = false;
     this.moveLeft = false;
     this.moveUp = false;
     this.moveDown = false;
-    document.querySelector('.game').append(this.image);
+    document.querySelector(".game").append(this.image);
   }
 
   drawSprite() {
     ctx.fillRect(this.x, this.y, this.width, this.height);
     this.image.style.top = `${this.y - 10}px`;
     this.image.style.left = `${this.x}px`;
-    
   }
 }
 
@@ -96,8 +101,42 @@ class Player extends GameSprite {
   }
 }
 
-class Enemy extends GameSprite{
+class Enemy extends GameSprite {
+  chooseDirection(currentMoveIndex) {
+    const diff = getRandomInt(-1, 2);
+    const newIndex = Math.abs(
+      currentMoveIndex + diff > pounts.length
+        ? currentMoveIndex - 1
+        : currentMoveIndex + diff
+    );
+    console.log(diff, currentMoveIndex);
+    this.nextPoint = pounts[newIndex];
+    this.currentPoint = pounts[currentMoveIndex];
+    this.x = this.nextPoint[0];
+    this.y = this.nextPoint[1];
 
+  }
+  move() {
+    const [x1, y1] = this.currentPoint;
+    const [x2, y2] = this.nextPoint;
+    if (x1 < x2) {
+      this.currentPoint[0] += 1;
+    }
+    if (x1 > x2) {
+      this.currentPoint[0] -= 1;
+    }
+    if (y1 < y2) {
+      this.currentPoint[1] += 1;
+    }
+    if (y1 > y2) {
+      this.currentPoint[1] -= 1;
+    }
+    this.x = x1;
+    this.y = y1;
+    if (this.currentPoint.toString() === this.nextPoint.toString()) {
+      this.chooseDirection(pounts.indexOf(this.currentPoint));
+    }
+  }
 }
 const isCollide = (obj1, obj2) => {
   const dx = (obj1.width + obj2.width) / 2;
@@ -107,8 +146,10 @@ const isCollide = (obj1, obj2) => {
   const centerY1 = obj1.y + obj1.height / 2;
   const centerY2 = obj2.y + obj2.height / 2;
 
-
-  if (Math.abs(centerX1 - centerX2) < dx && Math.abs(centerY1 - centerY2) < dy) {
+  if (
+    Math.abs(centerX1 - centerX2) < dx &&
+    Math.abs(centerY1 - centerY2) < dy
+  ) {
     console.log(`x1: ${obj1.x}`, `x2: ${obj2.x}`);
     return true;
   }
@@ -124,31 +165,42 @@ const isCollideObjects = (list) => {
   return false;
 };
 const walls = [];
-const pounts = [[710, 100], [710, 650]];
+const pounts = [
+  [710, 100],
+  [710, 650],
+  [515, 580],
+  [515, 100],
+  [310, 100],
+  [900, 100],
+  [900, 580],
+  [1100, 580],
+];
 const player = new Player(0, 0, "Player.png", 100, 100);
-const point1 = new GameSprite(710, 100,'', 5, 5);                //
-const point2 = new GameSprite(710, 580,'', 5, 5);                 //
-const point3 = new GameSprite(515, 580,'', 5, 5);                 //
-const point4 = new GameSprite(515, 100,'', 5, 5);                 //
-const point5 = new GameSprite(310, 100,'', 5, 5);
-const point6 = new GameSprite(310, 580,'', 5, 5);
-const point7 = new GameSprite(900, 100,'', 5, 5);
-const point8 = new GameSprite(900, 580,'', 5, 5);
-const point9 = new GameSprite(1100, 580,'', 5, 5);
-const wall1 = new GameSprite(200, 30, '', 20, 500);
-const wall2 = new GameSprite(200, 30, '', 800, 20);
-const wall3 = new GameSprite(200, 650, '', 800, 20);
-const wall4 = new GameSprite(400, 150, '', 20, 500);
-const wall5 = new GameSprite(600, 30, '', 20, 500);
-const wall6 = new GameSprite(800, 150, '', 20, 500);
+const point1 = new GameSprite(710, 100, "", 5, 5); //
+const point2 = new GameSprite(710, 580, "", 5, 5); //
+const point3 = new GameSprite(515, 580, "", 5, 5); //
+const point4 = new GameSprite(515, 100, "", 5, 5); //
+const point5 = new GameSprite(310, 100, "", 5, 5);
+const point6 = new GameSprite(310, 580, "", 5, 5);
+const point7 = new GameSprite(900, 100, "", 5, 5);
+const point8 = new GameSprite(900, 580, "", 5, 5);
+const point9 = new GameSprite(1100, 580, "", 5, 5);
+const wall1 = new GameSprite(200, 30, "", 20, 500);
+const wall2 = new GameSprite(200, 30, "", 800, 20);
+const wall3 = new GameSprite(200, 650, "", 800, 20);
+const wall4 = new GameSprite(400, 150, "", 20, 500);
+const wall5 = new GameSprite(600, 30, "", 20, 500);
+const wall6 = new GameSprite(800, 150, "", 20, 500);
 
+const enemy = new Enemy(710, 580, 'Pudge_2d.png', 50, 50);
+enemy.chooseDirection(2)
 
-walls.push(wall1)
-walls.push(wall2)
-walls.push(wall3)
-walls.push(wall4)
-walls.push(wall5)
-walls.push(wall6)
+walls.push(wall1);
+walls.push(wall2);
+walls.push(wall3);
+walls.push(wall4);
+walls.push(wall5);
+walls.push(wall6);
 
 player.startKeysEvents();
 
@@ -159,6 +211,8 @@ const render = () => {
     i.drawSprite();
   }
   player.movePlayer();
+  enemy.move();
+  enemy.drawSprite();
   point1.drawSprite();
   point2.drawSprite();
   point3.drawSprite();
